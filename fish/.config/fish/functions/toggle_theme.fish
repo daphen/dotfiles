184941cc635db107986ae40561f0c72ca93367d1
Daphen <davidkarlssson@gmail.com>
2026-03-08 00:09:28 +0100

    Fix toggle_theme: use theme-manager.sh to toggle all apps, not just fish

--- fish/.config/fish/functions/toggle_theme.fish
+++ fish/.config/fish/functions/toggle_theme.fish
@@ -1,19 +1,13 @@
 function toggle_theme -d "Toggle between light and dark themes"
-    # Get current theme from file
-    set -l current_theme "light"
-    if test -f ~/.config/theme_mode
-        set current_theme (cat ~/.config/theme_mode)
+    # Use the centralized theme manager to toggle all apps
+    bash ~/.config/themes/theme-manager.sh toggle
+    
+    # Source the new fish theme in this shell
+    set -l new_theme (cat ~/.config/theme_mode)
+    if test -f ~/.config/themes/generated/fish/$new_theme.theme
+        source ~/.config/themes/generated/fish/$new_theme.theme
     end
-
-    # Toggle the theme
-    if test "$current_theme" = "dark"
-        # Switch to light mode
-        echo "light" > ~/.config/theme_mode
-        set_light_theme
-    else
-        # Switch to dark mode
-        echo "dark" > ~/.config/theme_mode
-        set_dark_theme
+    if test -f ~/.config/themes/generated/tide/$new_theme.theme
+        source ~/.config/themes/generated/tide/$new_theme.theme
     end
-
 end

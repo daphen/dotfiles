7e140838ab9901e0df88ba2356b1e0a2ca7b40c9
David Karlsson <davidkarlssson@gmail.com>
2026-06-10 09:45:56 +0200

    fs-monitor: configure with default session watching cwd

--- nvim/.config/nvim/lua/plugins/fs-monitor.lua
+++ nvim/.config/nvim/lua/plugins/fs-monitor.lua
@@ -1,16 +1,35 @@
 return {
 	"bassamsdata/fs-monitor.nvim",
 	event = "VeryLazy",
-	opts = {
-		monitor = {
-			debounce_ms = 300,
-			respect_gitignore = true,
-			max_depth = 6,
-			max_file_size = 1024 * 1024 * 2,
-		},
-	},
+	config = function()
+		local fm = require("fs-monitor")
+		fm.setup({
+			monitor = {
+				debounce_ms = 300,
+				respect_gitignore = true,
+				max_depth = 6,
+				max_file_size = 1024 * 1024 * 2,
+			},
+		})
+		-- One default session per nvim instance watching cwd.
+		vim.g._fs_monitor_session = fm.create_session()
+		fm.start(vim.g._fs_monitor_session, vim.fn.getcwd())
+	end,
 	keys = {
-		{ "<leader>fm", function() require("fs-monitor").show_changes() end, desc = "fs-monitor: show changes" },
-		{ "<leader>fM", function() require("fs-monitor").create_checkpoint(nil, vim.fn.input("checkpoint label: ")) end, desc = "fs-monitor: checkpoint" },
+		{
+			"<leader>fm",
+			function()
+				require("fs-monitor").show_diff(vim.g._fs_monitor_session)
+			end,
+			desc = "fs-monitor: show diff",
+		},
+		{
+			"<leader>fM",
+			function()
+				local label = vim.fn.input("checkpoint label: ")
+				require("fs-monitor").create_checkpoint(vim.g._fs_monitor_session, label)
+			end,
+			desc = "fs-monitor: checkpoint",
+		},
 	},
 }
